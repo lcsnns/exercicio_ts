a232350281372db1d22439eb50a3a52482695590
lucas nunes <145053610+lcsnns@users.noreply.github.com>
2024-04-03 07:10:24 -0300

    commit inicial

---
+++ src/ex.js
@@ -0,0 +1,12 @@
+"use strict";
+// Função de multiplicação
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+// Exemplo de uso das funções
+console.log(multiplicacao(5, 3)); // Saída: 15
+console.log(saudacao("João")); // Saída: Olá João
---
+++ src/ex.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicacao(numero1: number, numero2: number): number {
+    return numero1 * numero2;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplo de uso das funções
+console.log(multiplicacao(5, 3)); // Saída: 15
+console.log(saudacao("João")); // Saída: Olá João
